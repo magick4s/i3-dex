76188b3f95e9a2f42bbbf87317d58dbe8a38ff8d
Jirrez Matheus <jirrezdex@gmail.com>
2019-02-02 18:09:14 -0200

    MD: rc.conf & controle.sh

--- config/scripts/controle.sh
+++ config/scripts/controle.sh
@@ -2,11 +2,9 @@
 # jirrezdex@gmail.com
 # Jirrez Matheus
 
-res=$(echo "-Bloquear;-Sair;-Reiniciar;-Desligar" | rofi -sep ";" -dmenu -p "Controle do sistema" -bw 1 -separator-style none -location 0 -width 20 -lines 4 -padding 5)
+res=$(echo "-Sair;-Reiniciar;-Desligar" | rofi -sep ";" -dmenu -p "Controle do sistema" -bw 1 -separator-style none -location 0 -width 20 -lines 4 -padding 5)
 if [ ! -z $res ]; then
   case $res in
-    -Bloquear)
-    ;;
     -Sair)
       i3-msg exit
     ;;

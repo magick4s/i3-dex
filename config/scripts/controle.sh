548cde3cbad54f2b5e0cae5a0c32630e8dfcaca8
Jirrez Matheus <jirrezdex@gmail.com>
2019-02-11 15:11:47 -0200

    MD: dunstrc, i3, polybar, rofi, controle.sh & termite

--- config/scripts/controle.sh
+++ config/scripts/controle.sh
@@ -2,7 +2,7 @@
 # jirrezdex@gmail.com
 # Jirrez Matheus
 
-res=$(echo "-Sair;-Reiniciar;-Desligar" | rofi -sep ";" -dmenu -p "Controle do sistema" -bw 2 -separator-style none -location 0 -width 20 -lines 3 -padding 5)
+res=$(echo "-Sair;-Reiniciar;-Desligar" | rofi -sep ";" -dmenu -p "Controle do sistema" -separator-style none -location 0 -width 40 -lines 3 -padding 40)
 if [ ! -z $res ]; then
   case $res in
      -Sair)

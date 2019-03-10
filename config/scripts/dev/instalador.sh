a924e2c868c324b0797d2f29242ecbab70053178
Jirrez Matheus <jirrezdex@gmail.com>
2019-03-10 16:21:36 -0300

    MD: config, rc.conf & instalador.sh

--- config/scripts/dev/instalador.sh
+++ config/scripts/dev/instalador.sh
@@ -31,7 +31,7 @@ echo -e "==> Olá "$USER", a instalação do I3-Dex está sendo inciada!"
 
 echo -e "==> Instalado pacotes essenciais"
 	# Instalado pacotes com o "pacman"
-		sudo pacman -S --needed --noconfirm alsa-utils git go i3-gaps pulseaudio pulseaudio-alsa termite ttf-dejavu vim xorg-server xorg-xinit xorg-xrandr powerline-fonts ttf-fira-mono ttf-fira-sans hsetroot compton dunst rofi ranger libnotify maim ffmpeg networkmanager
+		sudo pacman -S --needed --noconfirm alsa-utils git go i3-gaps pulseaudio pulseaudio-alsa termite ttf-dejavu vim xorg-server xorg-xinit xorg-xrandr powerline-fonts ttf-fira-mono ttf-fira-sans hsetroot compton dunst rofi ranger libnotify maim ffmpeg networkmanager w3m mpv
 
 	# Instalando "YAY"
 		git clone https://aur.archlinux.org/yay.git
@@ -41,7 +41,7 @@ echo -e "==> Instalado pacotes essenciais"
 		sudo rm -dR yay
 
 	# Instalado pacotes com o "YAY"
-		yay -S --needed --noconfirm google-chrome polybar nerd-fonts-source-code-pro
+		yay -S --needed --noconfirm google-chrome polybar nerd-fonts-source-code-pro pqiv
 
 	# Instalando gerenciador de login "LY"
 		git clone --recurse-submodules https://github.com/cylgom/ly.git
